0d9ea41eae8d4516c1b812f9d9714a189bdb1ebe
Christian <christiandouge90@gmail.com>
2026-03-28 15:03:12 -0700

    Changement minimal sur employeeform

---
+++ src/Components/EmployeeForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./EmployeeForm.css";
+
+function EmployeeForm({ addEmployee }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    position: "",
+    department: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formData.name || !formData.position || !formData.department) {
+      alert("Tous les champs sont requis !");
+      return;
+    }
+
+    addEmployee(formData);
+
+
+    setFormData({
+      name: "",
+      position: "",
+      department: ""
+    });
+  };
+
+  return (
+    <form className="employee-form" onSubmit={handleSubmit}>
+      <h2>Ajouter un employé</h2>
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Nom"
+        value={formData.name}
+        onChange={handleChange}
+      />
+
+      <input
+        type="text"
+        name="position"
+        placeholder="Poste"
+        value={formData.position}
+        onChange={handleChange}
+      />
+
+      <input
+        type="text"
+        name="department"
+        placeholder="Département"
+        value={formData.department}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Ajouter</button>
+    </form>
+  );
+}
+
+export default EmployeeForm;
