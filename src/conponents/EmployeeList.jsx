0aa5ac8609ecce47016ba89424d1b1272742abaa
Keyrozz <juniorapple13579@gmail.com>
2026-03-28 13:57:39 -0400

    Added employee list

---
+++ src/conponents/EmployeeList.jsx
@@ -0,0 +1,21 @@
+import EmployeeCard from "./EmployeeCard";
+import "./employeeList.css";
+
+export default function EmployeeList({ employees }) {
+    return (
+
+        <>
+        <div className="employee-list-containder">
+           {employees.map((employee, id) => (
+             <EmployeeCard
+               key={id}
+               name={employee.name}
+               position={employee.position}
+               department={employee.department}
+               status={employee.status}
+             />
+           ))}
+        </div>
+        </>
+    )
+}
