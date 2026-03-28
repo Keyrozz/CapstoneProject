f7cc9d6126a4b1793cce1be70d6ed373228d0f35
Keyrozz <juniorapple13579@gmail.com>
2026-03-28 16:24:27 -0400

    minimal changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import EmployeeList from './Components/EmployeeList.jsx';
+import { initialEmployees } from "./Data/Data.js"
 import './App.css'
 
 function App() {
   return (
     <>
+    <EmployeeList employees={initialEmployees} />
     </>
   )
 }
