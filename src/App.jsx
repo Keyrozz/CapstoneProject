c1bd90c0a69520252a8c5ce01dfa4fce81c52234
Christian <christiandouge90@gmail.com>
2026-03-28 16:01:56 -0700

    Changes to employeeform and appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import EmployeeList from './components/EmployeeList';
+import EmployeeForm from './Components/EmployeeForm';
 import './App.css'
 
 function App() {
   return (
     <>
+    <div className="app-container">
+      <div className="app-top">
+        <h1 className="app-title">Employee Directory</h1>
+        <span className="subtitle">All employees across departments</span>
+      </div>
+      <EmployeeList employees={[]} />
+      <EmployeeForm />
+    </div>
     </>
   )
 }
